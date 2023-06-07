d34117080c3a007c5bce6c930ba06c12546a693f
Fernando Caye <fdcaye@gmail.com>
2023-06-07 21:02:57 +0900

    Replace Assets and ProjectSettings folders with the sample project downloaded from the tutorial.

---
+++ Assets/Scripts/Common/Aspect Container/State Machine/StateMachine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace TheLiquidFire.AspectContainer
+{
+	public class StateMachine : Aspect {
+
+		public delegate void StateChangeHandler (IState fromState, IState toState);
+		public event StateChangeHandler didChangeStateEvent;
+
+		public IState currentState { get; private set; }
+
+		public void ChangeState<T> () where T : class, IState, new () {
+			IState previousState = currentState;
+			T nextState = container.GetAspect<T> () ?? container.AddAspect<T> ();
+
+			if (previousState != null) {
+				if (previousState == nextState || previousState.CanTransition(nextState) == false)
+					return;
+				previousState.Exit ();
+			}
+
+			currentState = nextState;
+
+			var handler = didChangeStateEvent;
+			if (handler != null)
+				handler (previousState, nextState);
+
+			nextState.Enter ();
+		}
+	}
+}
